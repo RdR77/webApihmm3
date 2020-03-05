93a4adff4be36b5f2a37f14fafc88f4dd418451e
RdR <RdR@DESKTOP-UQ4LR6L>
2020-03-05 08:11:27 -0300

    Add project files.

---
+++ webApihmm/Models/ParaHacer.cs
@@ -0,0 +1,14 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webApihmm.Models
+{
+    public class ParaHacer
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
